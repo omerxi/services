b94169be3c8643656656fd5321d328d03c85435b
Arie Benichou <arie.benichou@gmail.com>
2015-05-29 12:55:11 +0200

    project skeleton

---
+++ sms/index.js
@@ -0,0 +1,14 @@
+var colors = require('colors/safe');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(colors.green('listening at http://%s:%s'), host, port);
+});
